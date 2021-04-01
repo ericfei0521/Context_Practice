68a41557cfc20e0f300af97952e7c925ea4bbaaf
ericfei <ericfei@glaibo.com>
2021-04-01 15:50:50 +0800

    refactor: step1-1 : setting initial global state / testing reducer function correctly

---
+++ src/reducer.ts
@@ -0,0 +1,22 @@
+const initialNumber: { [index: string]: number } = {
+    "0": 0,
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0,
+    "6": 0,
+    "7": 0,
+    "8": 0,
+    "9": 0,
+}
+export const numberSwitcher = (state: { [index: string]: number } = initialNumber, action: any) => {
+    switch (action.type) {
+        case "SELECT_NUMBER":
+            return {
+                ...state,
+                [0]: 1
+            }
+        default: return state
+    }
+}
---
+++ src/test/selectNum.test.ts
@@ -0,0 +1,20 @@
+import { numberSwitcher } from '../reducer'
+
+describe('test number select test', () => {
+    it('select number 1 should return 1 ', () => {
+        const initialNumber: { [index: string]: number } = {
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9
+        }
+        const result = numberSwitcher(initialNumber, { type: 'SELECT_NUMBER' })
+        expect(result[0]).toBe(1)
+    })
+})
